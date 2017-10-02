8c374668a1fed09eead85c6598a1dc259f55a595
thedomeplatform <thedomeplatform@yandex.ru>
2017-10-02 06:02:17 +0000

    Adding timers for bonus stages

--- static/js/main.js
+++ static/js/main.js
@@ -130,7 +130,7 @@
     
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Oct 2, 2017 15:00:00 GMT+09:00").getTime();
+    var countDownDate = new Date("Oct 3, 2017 15:00:00 GMT+09:00").getTime();
     
     // Update the count down every 1 second
     var x = setInterval(function() {
@@ -148,7 +148,7 @@
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
       // Display the result in the element with id="demo"
-      document.getElementById("demo").innerHTML = "Crowdsale starts in: " + days + "d " + hours + "h "
+      document.getElementById("demo").innerHTML = "First Bonus Period ends in: " + days + "d " + hours + "h "
       + minutes + "m " + seconds + "s ";
     
       // If the count down is finished, write some text 
